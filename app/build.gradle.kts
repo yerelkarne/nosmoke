76ecb13e0ae7498d25ebe7d0f63d3a6ed9d48616
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-07 14:01:52 +0300

    Add Compose compiler plugin for Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
@@ -33,10 +34,6 @@ android {
 
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     kotlinOptions {
