1ccb16829e85cd6d60b562356fe82751b4f87707
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-07 18:03:02 +0300

    Fix MainActivity state delegate import

--- app/src/main/java/com/leosoft/smokefree/MainActivity.kt
+++ app/src/main/java/com/leosoft/smokefree/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.remember
