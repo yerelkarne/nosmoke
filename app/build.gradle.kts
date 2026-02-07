e96fd1689136283fcebdc22e91301e62baced65b
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-07 14:07:34 +0300

    Fix Material3 theme resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
