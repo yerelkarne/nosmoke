62a28f917464557f938cb570f09fe3972e1e1e84
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-09 09:11:48 +0300

    Match banner container to ad size

--- app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
+++ app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.height
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
@@ -12,12 +14,13 @@ import com.google.android.gms.ads.AdView
 fun BannerAd(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val adUnitIdValue = "ca-app-pub-3940256099942544/6300978111"
+    val adSize = AdSize.LARGE_BANNER
 
     AndroidView(
-        modifier = modifier,
+        modifier = modifier.height(adSize.height.dp),
         factory = { ctx ->
             AdView(ctx).apply {
-                setAdSize(AdSize.LARGE_BANNER)
+                setAdSize(adSize)
                 setAdUnitId(adUnitIdValue)
                 loadAd(AdRequest.Builder().build())
             }
