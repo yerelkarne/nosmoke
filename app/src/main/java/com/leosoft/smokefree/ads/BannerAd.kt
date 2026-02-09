830ba2fb0714910c78ab003dc4b7aa4e3766bc5e
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-09 09:06:50 +0300

    Remove banner height padding

--- app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
+++ app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
@@ -4,8 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.height
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
@@ -16,7 +14,7 @@ fun BannerAd(modifier: Modifier = Modifier) {
     val adUnitIdValue = "ca-app-pub-3940256099942544/6300978111"
 
     AndroidView(
-        modifier = modifier.height(100.dp),
+        modifier = modifier,
         factory = { ctx ->
             AdView(ctx).apply {
                 setAdSize(AdSize.LARGE_BANNER)
