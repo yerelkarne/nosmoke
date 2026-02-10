ba004084c71985d8ef40f5f6b0a1df78730f702c
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-10 22:33:57 +0300

    Hide banner container until ad is loaded

--- app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
+++ app/src/main/java/com/leosoft/smokefree/ads/BannerAd.kt
@@ -1,7 +1,5 @@
 package com.leosoft.smokefree.ads
 
-import android.graphics.Color
-import android.view.ViewGroup
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,7 +7,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdListener
@@ -21,27 +18,21 @@ import com.google.android.gms.ads.AdView
 fun BannerAd(modifier: Modifier = Modifier) {
     val adUnitIdValue = "ca-app-pub-3940256099942544/6300978111"
     val requestedAdSize = AdSize.LARGE_BANNER
-    val density = LocalDensity.current
-    var bannerHeightDp by remember { mutableStateOf(requestedAdSize.height.dp) }
+    var bannerHeightDp by remember { mutableStateOf(0.dp) }
 
     AndroidView(
         modifier = modifier.height(bannerHeightDp),
         factory = { context ->
             AdView(context).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.WRAP_CONTENT
-                )
-                setBackgroundColor(Color.TRANSPARENT)
                 setAdSize(requestedAdSize)
                 setAdUnitId(adUnitIdValue)
                 adListener = object : AdListener() {
                     override fun onAdLoaded() {
-                        val loadedHeightPx = adSize?.getHeightInPixels(context)
-                            ?: height
-                        if (loadedHeightPx > 0) {
-                            bannerHeightDp = with(density) { loadedHeightPx.toDp() }
-                        }
+                        bannerHeightDp = requestedAdSize.height.dp
+                    }
+
+                    override fun onAdFailedToLoad(error: com.google.android.gms.ads.LoadAdError) {
+                        bannerHeightDp = 0.dp
                     }
                 }
                 loadAd(AdRequest.Builder().build())
