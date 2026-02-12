486b51ac32369fc315e9b6904cd71372f4e697d4
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-02-12 22:11:15 +0300

    Fix missing height import in AppComponents

--- app/src/main/java/com/leosoft/smokefree/ui/AppComponents.kt
+++ app/src/main/java/com/leosoft/smokefree/ui/AppComponents.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
